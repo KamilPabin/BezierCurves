9a1fb5ebc9693159fc3a0d6a1854087958be5379
Kamil Pabin <pabin.kamil@gmail.com>
2022-06-04 22:04:40 +0200

    Scale image with window

--- Bezier.js
+++ Bezier.js
@@ -22,30 +22,30 @@ const resize = e => {
 
 const P0 = {
     name: 'P0',
-    x: 100,
-    y: 200,
-    r: 5
+    x: 0.1,
+    y: 0.1,
+    r: 0.002,
 }
 
 const P1 = {
     name: 'P1',
-    x: 300,
-    y: 600,
-    r: 5
+    x: 0.1,
+    y: 0.9,
+    r: 0.002,
 }
 
 const P2 = {
     name: 'P2',
-    x: 2000,
-    y: 100,
-    r: 5
+    x: 0.9,
+    y: 0.1,
+    r: 0.002,
 }
 
 const P3 = {
     name: 'P3',
-    x: 900,
-    y: 1200,
-    r: 5
+    x: 0.9,
+    y: 0.9,
+    r: 0.002,
 }
 
 const POINTS = [P0, P1, P2, P3]
@@ -67,10 +67,10 @@ function draw() {
     drawLine(ctx, P2, P3)
 
     ctx.beginPath();
-    ctx.moveTo(P0.x, P0.y);
+    ctx.moveTo(P0.x * window.innerWidth, P0.y * window.innerHeight);
     for (let t = 0; t < 1; t = t + 0.01) {
         const point = newPoint(t)
-        ctx.lineTo(point.x, point.y)
+        ctx.lineTo(point.x * window.innerWidth, point.y * window.innerHeight)
     }
     ctx.stroke()
     ctx.closePath()
@@ -79,29 +79,29 @@ function draw() {
 function newPoint(t) {
     return {
         x: P0.x * P0cf(t) + P1.x * P1cf(t) + P2.x * P2cf(t) + P3.x * P3cf(t),
-        y: P0.y * P0cf(t) + P1.y * P1cf(t) + P2.y * P2cf(t) + P3.y * P3cf(t)
+        y: P0.y * P0cf(t) + P1.y * P1cf(t) + P2.y * P2cf(t) + P3.y * P3cf(t),
     }
 }
 
 function drawCircle(ctx, p) {
     ctx.beginPath()
-    ctx.arc(p.x, p.y, p.r, 0, 2 * Math.PI)
+    ctx.arc(p.x * window.innerWidth, p.y * window.innerHeight, p.r * window.innerWidth, 0, 2 * Math.PI)
     ctx.closePath()
     ctx.fill()
 }
 
 function drawLine(ctx, p0, p1) {
     ctx.beginPath()
-    ctx.moveTo(p0.x, p0.y)
-    ctx.lineTo(p1.x, p1.y)
+    ctx.moveTo(p0.x * window.innerWidth, p0.y * window.innerHeight)
+    ctx.lineTo(p1.x * window.innerWidth, p1.y * window.innerHeight)
     ctx.closePath()
     ctx.stroke()
 }
 
 canvas.addEventListener('mousedown', e => {
     isDragging = true
-    let x = e.offsetX
-    let y = e.offsetY
+    let x = e.offsetX / window.innerWidth
+    let y = e.offsetY / window.innerHeight
     for (let p of POINTS) {
         if (isOverThePoint(p, x, y)) {
             draggingPoint = p
@@ -119,8 +119,8 @@ canvas.addEventListener('mousemove', e => {
     if (!isDragging || draggingPoint === undefined) {
         return
     }
-    draggingPoint.x = e.offsetX
-    draggingPoint.y = e.offsetY
+    draggingPoint.x = e.offsetX / window.innerWidth
+    draggingPoint.y = e.offsetY / window.innerHeight
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     draw()
 })
